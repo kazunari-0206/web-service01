4a2be04ba31c5bbc9f93f2f43a0871a9f364a1b9
kazunari.kaneko <kaneko.kazunari0206@gmail.com>
2020-02-29 19:36:32 +0900

    パスワード変更メッセージのjs追加

--- 01/footer.php
+++ 01/footer.php
@@ -9,6 +9,13 @@
     if( window.innerHeight > $ftr.offset().top + $ftr.outerHeight() ){
       $ftr.attr({'style': 'position:fixed; top:' + (window.innerHeight - $ftr.outerHeight()) +'px;' });
     }
+    // メッセージ表示
+    var $jsShowMsg = $('#js-show-msg');
+    var msg = $jsShowMsg.text();
+    if(msg.replace(/^[\s　]+|[\s　]+$/g, "").length) {
+      $jsShowMsg.slideToggle('slow');
+      setTimeout(function(){$jsShowMsg.slideToggle('slow');}, 5000);
+    }
   });
 </script>
 </body>
