92e336ba9c2fb7122626e950f819e00cf8730055
kazunari.kaneko <kaneko.kazunari0206@gmail.com>
2020-03-01 00:51:18 +0900

    07画面処理完成

--- 01/passRemindRecieve.php
+++ 01/passRemindRecieve.php
@@ -1,4 +1,116 @@
 <?php
+
+//共通変数・関数ファイルを読み込み
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('パスワード再発行認証キー入力ページ ');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+//ログイン認証はなし（ログインできない人が使う画面なので）
+
+//SESSIONに認証キーがあるか確認、なければリダイレクト
+if(empty($_SESSION['auth_key'])){
+  header("Location:passRemindSend.php"); //認証キー送信ページへ
+}
+
+//================================
+// 画面処理
+//================================
+//post送信されていた場合
+if(!empty($_POST)){
+  debug('POST送信があります。');
+  debug('POST情報 :'.print_r($_POST, true));
+
+  //変数に認証キーを代入
+  $auth_key = $_POST['token'];
+
+  //未入力チェック
+  validRequired($auth_key, 'token');
+
+  if(empty($err_msg)){
+    debug('未入力チェックOK。');
+
+    //固定長チェック
+    validLength($auth_key, 'token');
+    //半角チェック
+    validHalf($auth_key, 'token');
+
+    if(empty($err_msg)){
+      debug('バリデーションOK。');
+
+      if($auth_key !== $_SESSION['auth_key']){
+        $err_msg['common'] = MSG15;
+      }
+      if(time() > $_SESSION['auth_key_limit']){
+        $err_msg['common'] = MSG16;
+      }
+
+      if(empty($err_msg)){
+        debug('認証OK。');
+
+        $pass = makeRandKey();
+
+        //例外処理
+        try {
+          // DBへ接続
+          $dbh = dbConnect();
+          // SQL文作成
+          $sql = 'UPDATE users SET password = :pass WHERE email = :email AND delete_flg = 0';
+          $data = array(':email' => $_SESSION['auth_email'], ':pass' => password_hash($pass, PASSWORD_DEFAULT));
+          // クエリ実行
+          $stmt = queryPost($dbh, $sql, $data);
+
+          // クエリ成功の場合
+          if($stmt){
+            debug('クエリ成功。');
+
+            //メールを送信
+            $from = 'kanekazu0206@gmail.com';
+            $to = $_SESSION['auth_email'];
+            $subject = '【パスワード再発行完了】 | WEBUKATUMARKET';
+            // EOTはEndOfTextの略。ABCでもなんでもいい。先頭の<<<のあとの文字列と合わせること。最後のEOTの前後に空白など何も入れてはいけない。
+          // EOT内の半角空白も全てそのまま半角空白として扱われるのでインデントはしないこと。
+          $comment = <<<EOT
+本メールアドレス宛にパスワードの再発行を致しました。
+下記のURLにて再発行パスワードをご入力頂き、ログインください。
+
+ログインページ : http://localhost:8888/git-webservice/01/login.php
+再発行パスワード : {$pass}
+※ログイン後、パスワードのご変更をお願い致します。
+
+/////////////////////////////////////////////////////////////
+ウェブカツマーケットカスタマーセンター
+URL  http://webukatu.com/
+E-mail info@webukatu.com
+////////////////////////////////////////////////////////////
+EOT;
+            sendMail($from, $to, $subject, $comment);
+
+            //セッション削除
+            session_unset();
+            $_SESSION['msg_success'] = SUC03;
+            debug('セッション変数の中身 :' .print_r($_SESSION, true));
+
+            header("Location:login.php"); //ログインページへ
+
+          }else{
+            debug('クエリに失敗しました。');
+            $err_msg['common'] = MSG07;
+          }
+
+        } catch (Exception $e) {
+          error_log('エラー発生 :' . $e->getMessage());
+          $err_msg['common'] = MSG07;
+        }
+      }
+    }
+  }
+
+}
+?>
+<?php
 $siteTitle = 'パスワード再発行認証';
 require('head.php'); 
 ?>
