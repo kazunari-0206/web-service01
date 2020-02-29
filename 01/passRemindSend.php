07f71276d56f7b26763e13984514c31b3edc92d9
kazunari.kaneko <kaneko.kazunari0206@gmail.com>
2020-03-01 00:16:18 +0900

    07完成

--- 01/passRemindSend.php
+++ 01/passRemindSend.php
@@ -50,7 +50,7 @@ if(!empty($_POST)){
         // EmailがDBに登録されている場合
         if($stmt && array_shift($result)){
           debug('クエリ成功。DB登録あり。');
-          $_SESSION['msg_success'] = SUS03;
+          $_SESSION['msg_success'] = SUC03;
 
           $auth_key = makeRandKey(); //認証キー生成
 
@@ -121,12 +121,22 @@ require('head.php');
 
         <div class="form-container">
 
-          <form action="passRemindRecieve.php" class="form">
+          <form action="" method="post" class="form">
             <p>ご指定のメールアドレス宛にパスワード再発行用のURLと認証キーをお送り致します。</p>
-            <label>
+            <div class="area-msg">
+            <?php
+            echo getErrMsg('common');
+            ?>
+            </div>
+            <label class="<?php if(!empty($err_msg['email'])) echo 'err'; ?>">
               Email
-              <input type="text" name="email">
+              <input type="text" name="email" value="<?php echo getFormData('email'); ?>">
             </label>
+            <div class="area-msg">
+            <?php
+            echo getErrMsg('email');
+            ?>
+            </div>
             <div class="btn-container">
               <input type="submit" class="btn btn-mid" value="送信する">
             </div>
