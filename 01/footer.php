5b7c8b4829a44166beb02dcb2e641fd9727f0388
kazunari.kaneko <kaneko.kazunari0206@gmail.com>
2020-03-05 23:39:42 +0900

    ライブプレビュー、テキストカウント追加

--- 01/footer.php
+++ 01/footer.php
@@ -12,10 +12,48 @@
     // メッセージ表示
     var $jsShowMsg = $('#js-show-msg');
     var msg = $jsShowMsg.text();
-    if(msg.replace(/^[\s　]+|[\s　]+$/g, "").length) {
+    if(msg.replace(/^[\s　]+|[\s　]+$/g, "").length){
       $jsShowMsg.slideToggle('slow');
-      setTimeout(function(){$jsShowMsg.slideToggle('slow');}, 5000);
+      setTimeout(function(){ $jsShowMsg.slideToggle('slow'); }, 5000);
     }
+    
+    // 画像ライブプレビュー
+    var $dropArea = $('.area-drop');
+    var $fileInput = $('.input-file');
+    $dropArea.on('click', function(e){
+      e.stopPropagation();
+      e.preventDefault();
+      $(this).css('border', '3px #ccc dashed');
+    });
+    $dropArea.on('dragleave', function(e){
+      e.stopPropagation();
+      e.preventDefault();
+      $(this).css('border', 'none');
+    });
+    $fileInput.on('change', function(e){
+      $dropArea.css('border', 'none');
+      var file = this.files[0],            // 2. files配列にファイルが入っています
+          $img = $(this).siblings('.prev-img'), // 3. jQueryのsiblingsメソッドで兄弟のimgを取得
+          fileReader = new FileReader();   // 4. ファイルを読み込むFileReaderオブジェクト
+
+      // 5. 読み込みが完了した際のイベントハンドラ。imgのsrcにデータをセット
+      fileReader.onload = function(event) {
+        // 読み込んだデータをimgに設定
+        $img.attr('src', event.target.result).show();
+      };
+
+      // 6. 画像読み込み
+      fileReader.readAsDataURL(file);
+
+    });
+    
+    // テキストエリアカウント
+    var $countUp = $('#js-count'),
+        $countView = $('#js-count-view');
+    $countUp.on('keyup', function(e){
+      $countView.html($(this).val().length);
+    });
+    
   });
 </script>
 </body>
