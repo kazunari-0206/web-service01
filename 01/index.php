5d747f6a17af94762f7b6efb66c389582163b36c
kazunari.kaneko <kaneko.kazunari0206@gmail.com>
2020-03-09 12:12:31 +0900

    09完成

--- 01/index.php
+++ 01/index.php
@@ -1,4 +1,43 @@
 <?php
+
+//共通変数・関数ファイルを読み込み
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug(' トップページ ');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+//================================
+// 画面処理
+//================================
+
+// 画面表示用データ取得
+//================================
+// カレントページのGETパラメータを取得
+$currentPageNum = (!empty($_GET['p'])) ? $_GET['p'] : 1; // デフォルトは１ページめ
+// パラメータに不正な値が入っているかチェック
+if(!is_int((int)$currentPageNum)){
+  error_log('エラー発生:指定ページに不正な値が入りました');
+  header("Location:index.php"); //トップページへ
+  exit;
+}
+// 表示件数
+$listSpan = 20;
+// 現在の表示レコード先頭を算出
+$currentMinNum = (($currentPageNum-1)*$listSpan); //1ページ目なら(1-1)*20 = 0、２ページ目なら(2-1)*20 = 20
+// DBから商品データを取得
+$dbProductData = getProductList($currentMinNum);
+// DBからカテゴリデータを取得
+$dbCategoryData = getCategory();
+debug('現在のページ: '.$currentPageNum);
+//debug('フォーム用DBデータ:'.print_r($dbFormData, true));
+//debug('カテゴリデータ:'.print_r($dbCategoryData, true));
+
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<');
+?>
+
+<?php
 $siteTitle = 'HOME';
 require('head.php'); 
 ?>
@@ -41,152 +80,74 @@ require('head.php');
       <section id="main" >
         <div class="search-title">
           <div class="search-left">
-            <span class="total-num">104</span>件の商品が見つかりました
+            <span class="total-num"><?php echo sanitize($dbProductData['total']); ?></span>件の商品が見つかりました
           </div>
           <div class="search-right">
-            <span class="num">1</span> - <span class="num">20</span>件 / <span class="num">104</span>件中
+            <span class="num"><?php echo $currentMinNum+1; ?></span> - <span class="num"><?php echo $currentMinNum+$listSpan; ?></span>件 / <span class="num"><?php echo sanitize($dbProductData['total']); ?></span>件中
           </div>
         </div>
+
         <div class="panel-list">
-          <a href="productDetail.php" class="panel">
+          <?php
+            foreach($dbProductData['data'] as $key => $val):
+          ?>
+          <a href="productDetail.php?p_id=<?php echo $val['id']; ?>" class="panel">
             <div class="panel-head">
-              <img src="img/sample01.jpg" alt="商品タイトル">
+              <img src="<?php echo sanitize($val['pic1']); ?>" alt="<?php echo sanitize($val['name']); ?>">
             </div>
             <div class="panel-body">
-              <p class="panel-title">iPhone6s <span class="price">¥89,000</span></p>
+              <p class="panel-title"><?php echo sanitize($val['name']); ?><span class="price">¥<?php echo sanitize(number_format($val['price'])); ?></span></p>
             </div>
           </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample02.jpg" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">ASUS VivoBook E200HA <span class="price">¥75,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample06.jpg" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">MacBook Pro Retina <span class="price">¥89,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample04.jpg" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">ミスノ　クロスバイク <span class="price">¥29,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample03.gif" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">電動自転車 <span class="price">¥58,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample08.jpg" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">アイアンセット <span class="price">¥12,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample07.jpg" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">フィットネスマシン <span class="price">¥34,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample10.jpg" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">ウォーキングシューズ <span class="price">¥4,500</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample05.jpg" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">iPhone6s <span class="price">¥89,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample09.jpeg" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">ASUS VivoBook E200HA <span class="price">¥75,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample-img.png" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">MacBook Pro Retina <span class="price">¥89,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample-img.png" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">ミスノ　クロスバイク <span class="price">¥29,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample-img.png" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">電動自転車 <span class="price">¥58,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample-img.png" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">アイアンセット <span class="price">¥12,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample-img.png" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">フィットネスマシン <span class="price">¥34,000</span></p>
-            </div>
-          </a>
-          <a href="" class="panel">
-            <div class="panel-head">
-              <img src="img/sample-img.png" alt="商品タイトル">
-            </div>
-            <div class="panel-body">
-              <p class="panel-title">ウォーキングシューズ <span class="price">¥4,500</span></p>
-            </div>
-          </a>
+          <?php
+            endforeach;
+          ?>
         </div>
 
         <div class="pagination">
           <ul class="pagination-list">
-            <li class="list-item"><a href="">&lt;</a></li>
-            <li class="list-item"><a href="">1</a></li>
-            <li class="list-item"><a href="">2</a></li>
-            <li class="list-item active"><a href="">3</a></li>
-            <li class="list-item"><a href="">4</a></li>
-            <li class="list-item"><a href="">5</a></li>
-            <li class="list-item"><a href="">&gt;</a></li>
+            <?php
+              $pageColNum = 5;
+              $totalPageNum = $dbProductData['total_page'];
+              // 現在のページが、総ページ数と同じ かつ 総ページ数が表示項目数以上なら、左にリンク４個出す
+              if($currentPageNum == $totalPageNum && $totalPageNum >= $pageColNum){
+                $minPageNum = $currentPageNum-4;
+                $maxPageNum = $currentPageNum;
+                // 現在のページが、総ページ数の１ページ前なら、左にリンク３個、右に１個出す
+              }elseif($currentPageNum == ($totalPageNum-1) && $totalPageNum >= $pageColNum){
+                $minPageNum = $currentPageNum - 3;
+                $maxPageNum = $currentPageNum + 1;
+                // 現ページが２の場合は左にリンク１個、右にリンク３個出す。
+              }elseif($currentPageNum == 2 && $totalPageNum >= $pageColNum){
+                $minPageNum = $currentPageNum - 1;
+                $maxPageNum = $currentPageNum + 3;
+                // 現ページが１の場合は左に何も出さない。右に５個出す。
+              }elseif($currentPageNum == 1 && $totalPageNum >= $pageColNum){
+                $minPageNum = $currentPageNum;
+                $maxPageNum = 5;
+                // 総ページ数が表示項目数より少ない場合は、総ページ数をループのMax,ループのMinを１に設定
+              }elseif($totalPageNum < $pageColNum){
+                $minPageNum = 1;
+                $maxPageNum = $totalPageNum;
+                // それ以外は左に２個出す。
+              }else{
+                $minPageNum = $currentPageNum - 2;
+                $maxPageNum = $currentPageNum + 2;
+              }
+            ?>
+            <?php if($currentPageNum != 1): ?>
+              <li class="list-item"><a href="?p=1">&lt;</a></li>
+            <?php endif; ?>
+            <?php
+              for($i = $minPageNum; $i <= $maxPageNum; $i++):
+            ?>
+            <li class="list-item <?php if($currentPageNum == $i) echo 'active'; ?>"><a href="?p=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+            <?php
+              endfor;
+            ?>
+            <?php if($currentPageNum != $maxPageNum): ?>
+              <li class="list-item"><a href="?p=<?php echo $maxPageNum; ?>">&gt;</a></li>
+            <?php endif; ?>
           </ul>
         </div>
         
