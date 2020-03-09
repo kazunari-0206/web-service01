eda97967b33eb131b58e1ad26a8df4af0fb7d0fe
kazunari.kaneko <kaneko.kazunari0206@gmail.com>
2020-03-09 22:50:24 +0900

    12 先輩の回答をコピペ

--- 01/msg.php
+++ 01/msg.php
@@ -1,17 +1,113 @@
 <?php
+
 //共通変数・関数ファイルを読込み
 require('function.php');
 
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
-debug(' 連絡掲示板 ');
+debug(' 連絡掲示板ページ ');
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
 debugLogStart();
 
 //================================
 // 画面処理
 //================================
+$partnerUserId = '';
+$partnerUserInfo = '';
+$myUserInfo = '';
+$productInfo = '';
+// 画面表示用データ取得
+//================================
+// GETパラメータを取得
+$m_id = (!empty($_GET['m_id'])) ? $_GET['m_id'] : '';
+// DBから掲示板とメッセージデータを取得
+$viewData = getMsgsAndBord($m_id);
+debug('取得したDBデータ：'.print_r($viewData,true));
+// パラメータに不正な値が入っているかチェック
+if(empty($viewData)){
+  error_log('エラー発生:指定ページに不正な値が入りました');
+  header("Location:mypage.php"); //マイページへ
+    exit;
+}
+// 商品情報を取得
+if($viewData !== 1){
+$productInfo = getProductOne($viewData['product_id']);
+debug('取得したDBデータ：'.print_r($productInfo,true));
+// 商品情報が入っているかチェック
+if(empty($productInfo)){
+  error_log('エラー発生:商品情報が取得できませんでした');
+  header("Location:mypage.php"); //マイページへ
+    exit;
+}
+// viewDataから相手のユーザーIDを取り出す
+$dealUserIds[] = $viewData['sale_user'];
+$dealUserIds[] = $viewData['buy_user'];
+if(($key = array_search($_SESSION['user_id'], $dealUserIds)) !== false) {
+    unset($dealUserIds[$key]);
+} 
+$partnerUserId = array_shift($dealUserIds);
+debug('取得した相手のユーザーID：'.$partnerUserId);
+// DBから取引相手のユーザー情報を取得
+if(isset($partnerUserId)){
+  $partnerUserInfo = getUser($partnerUserId);
+}
+// 相手のユーザー情報が取れたかチェック
+if(empty($partnerUserInfo)){
+  error_log('エラー発生:相手のユーザー情報が取得できませんでした');
+  header("Location:mypage.php"); //マイページへ
+}
+// DBから自分のユーザー情報を取得
+$myUserInfo = getUser($_SESSION['user_id']);
+debug('取得したユーザデータ：'.print_r($partnerUserInfo,true));
+// 自分のユーザー情報が取れたかチェック
+if(empty($myUserInfo)){
+  error_log('エラー発生:自分のユーザー情報が取得できませんでした');
+  header("Location:mypage.php"); //マイページへ
+    exit;
+}}
 
+// post送信されていた場合
+if(!empty($_POST)){
+  debug('POST送信があります。');
+  
+  //ログイン認証
+  require('auth.php');
+  
+  //バリデーションチェック
+  $msg = (isset($_POST['msg'])) ? $_POST['msg'] : '';
+  //最大文字数チェック
+  validMaxLen($msg, 'msg', 500);
+  //未入力チェック
+  validRequired($msg, 'msg');
+  
+  if(empty($err_msg)){
+    debug('バリデーションOKです。');
 
+    //例外処理
+    try {
+      // DBへ接続
+      $dbh = dbConnect();
+      // SQL文作成
+      $sql = 'INSERT INTO message (bord_id, send_date, to_user, from_user, msg, create_date) VALUES (:b_id, :send_date, :to_user, :from_user, :msg, :date)';
+      $data = array(':b_id' => $m_id, ':send_date' => date('Y-m-d H:i:s'), ':to_user' => $partnerUserId, ':from_user' => $_SESSION['user_id'], ':msg' => $msg, ':date' => date('Y-m-d H:i:s'));
+      // クエリ実行
+      $stmt = queryPost($dbh, $sql, $data);
+
+      // クエリ成功の場合
+      if($stmt){
+        $_POST = array(); //postをクリア
+        debug('連絡掲示板へ遷移します。');
+        header("Location: " . $_SERVER['PHP_SELF'] .'?m_id='.$m_id); //自分自身に遷移する
+      }
+
+    } catch (Exception $e) {
+      error_log('エラー発生:' . $e->getMessage());
+      $err_msg['common'] = MSG07;
+    }
+  }
+}
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+?>
+<?php
 $siteTitle = '連絡掲示板';
 require('head.php'); 
 ?>
@@ -84,7 +180,7 @@ require('head.php');
         margin-bottom: 30px;
       }
       .area-bord .msg-cnt .avatar{
-        width: 5%;
+        width: 5.2%;
         overflow: hidden;
         float: left;
       }
@@ -144,106 +240,90 @@ require('head.php');
     <?php
       require('header.php'); 
     ?>
-
+    
     <p id="js-show-msg" style="display:none;" class="msg-slide">
       <?php echo getSessionFlash('msg_success'); ?>
     </p>
-
 
     <!-- メインコンテンツ -->
     <div id="contents" class="site-width">
       <!-- Main -->
       <section id="main" >
         <div class="msg-info">
+          <?php 
+            if($viewData !== 1){ 
+          ?>
           <div class="avatar-img">
-            <img src="img/avatar.png" alt="" class="avatar"><br>
+            <img src="<?php echo showImg(sanitize($partnerUserInfo['pic'])); ?>" alt="" class="avatar"><br>
           </div>
           <div class="avatar-info">
-            山田　太郎　33歳<br>
-            〒111-11111<br>
-            東京都墨田区◯◯◯１−１−１◯◯◯マンション１３０３号室<br>
-            TEL：000-0000-0000
+            <?php echo sanitize($partnerUserInfo['username']).' '.sanitize($partnerUserInfo['age']).'歳' ?><br>
+            〒<?php echo substr($partnerUserInfo['zip'],0,3); ?> - <?php echo substr($partnerUserInfo['zip'],3,4); ?><br>
+            <?php echo sanitize($partnerUserInfo['addr']); ?><br>
+            TEL：<?php echo sanitize($partnerUserInfo['tel']); ?>
           </div>
           <div class="product-info">
             <div class="left">
               取引商品<br>
-              <img src="img/sample01.jpg" alt="" height="70px" width="auto" >
+              <img src="<?php echo showImg(sanitize($productInfo['pic1'])); ?>" alt="" height="70px" width="auto" >
             </div>
             <div class="right">
-              iPhone6s<br>
-              取引金額：<span class="price">¥87,000</span><br>
-              取引開始日：2016/00/00
+              <?php echo sanitize($productInfo['name']); ?><br>
+              取引金額：<span class="price">¥<?php echo number_format(sanitize($productInfo['price'])); ?></span><br>
+              取引開始日：<?php echo date('Y/m/d', strtotime(sanitize($viewData['create_date']))); ?>
             </div>
           </div>
+          <?php } else{ ?>
+              <p>掲示板は消されています。</p>
+                
+            <?php } ?>
         </div>
         <div class="area-bord" id="js-scroll-bottom">
-          <div class="msg-cnt msg-left">
-            <div class="avatar">
-              <img src="img/avatar2.jpg" alt="" class="avatar">
-            </div>
-            <p class="msg-inrTxt">
-              <span class="triangle"></span>
-              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-            </p>
-          </div>
-          <div class="msg-cnt msg-right">
-            <div class="avatar">
-              <img src="img/avatar.png" alt="" class="avatar">
-            </div>
-            <p class="msg-inrTxt">
-              <span class="triangle"></span>
-              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキ
-            </p>
-          </div>
-          <div class="msg-cnt msg-left">
-            <div class="avatar">
-              <img src="img/avatar2.jpg" alt="" class="avatar">
-            </div>
-            <p class="msg-inrTxt">
-              <span class="triangle"></span>
-              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサ
-            </p>
-          </div>
-          <div class="msg-cnt msg-right">
-            <div class="avatar">
-              <img src="img/avatar.png" alt="" class="avatar">
-            </div>
-            <p class="msg-inrTxt">
-              <span class="triangle"></span>
-              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキ
-            </p>
-          </div>
-          <div class="msg-cnt msg-left">
-            <div class="avatar">
-              <img src="img/avatar2.jpg" alt="" class="avatar">
-            </div>
-            <p class="msg-inrTxt">
-              <span class="triangle"></span>
-              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサ
-            </p>
-          </div>
-          <div class="msg-cnt msg-right">
-            <div class="avatar">
-              <img src="img/avatar.png" alt="" class="avatar">
-            </div>
-            <p class="msg-inrTxt">
-              <span class="triangle"></span>
-              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキ
-            </p>
-          </div>
-          <div class="msg-cnt msg-left">
-            <div class="avatar">
-              <img src="img/avatar2.jpg" alt="" class="avatar">
-            </div>
-            <p class="msg-inrTxt">
-              <span class="triangle"></span>
-              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサ
-            </p>
-          </div>
+          <?php
+            if(!empty($viewData['msg'])){
+              foreach($viewData['msg'] as $key => $val){
+                  if(!empty($val['from_user']) && $val['from_user'] == $partnerUserId){
+          ?>
+                    <div class="msg-cnt msg-left">
+                      <div class="avatar">
+                        <img src="<?php echo sanitize(showImg($partnerUserInfo['pic'])); ?>" alt="" class="avatar">
+                      </div>
+                      <p class="msg-inrTxt">
+                        <span class="triangle"></span>
+                        <?php echo sanitize($val['msg']); ?>
+                      </p>
+                      <div style="font-size:.5em;"><?php echo sanitize($val['send_date']); ?></div>
+                    </div>
+            <?php
+                  }else{
+            ?>
+                    <div class="msg-cnt msg-right">
+                      <div class="avatar">
+                        <img src="<?php echo sanitize(showImg($myUserInfo['pic'])); ?>" alt="" class="avatar">
+                      </div>
+                      <p class="msg-inrTxt">
+                        <span class="triangle"></span>
+                        <?php echo sanitize($val['msg']); ?>
+                      </p>
+                      <div style="font-size:.5em;text-align:right;"><?php echo sanitize($val['send_date']); ?></div>
+                    </div>
+            <?php
+                  }
+                }
+              }elseif($viewData === 1){ ?> 
+                <p style="text-align:center;line-height:20;">掲示板は消されています。</p>
+              <?php } else{ ?>
+                <p style="text-align:center;line-height:20;">メッセージ投稿はまだありません</p>
+            <?php
+              }
+          ?>
+          
         </div>
         <div class="area-send-msg">
-          <textarea name="" id="" cols="30" rows="3"></textarea>
-          <input type="submit" value="送信" class="btn btn-send">
+          <form action="" method="post">
+            <textarea name="msg" cols="30" rows="3"></textarea>
+            <input type="submit" value="送信" class="btn btn-send">
+          </form>
         </div>
       </section>
       
@@ -251,6 +331,7 @@ require('head.php');
       
       <script>
         $(function(){
+          //scrollHeightは要素のスクロールビューの高さを取得するもの
           $('#js-scroll-bottom').animate({scrollTop: $('#js-scroll-bottom')[0].scrollHeight}, 'fast');
         });
       </script>
