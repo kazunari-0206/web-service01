b507d8c643d05dbf7d35f7251361b28bed4ded69
kazunari.kaneko <kaneko.kazunari0206@gmail.com>
2020-03-09 23:02:26 +0900

    13　お気に入り登録js追加

--- 01/footer.php
+++ 01/footer.php
@@ -5,6 +5,7 @@
 <script src="js/vendor/jquery-2.2.2.min.js"></script>
 <script>
   $(function(){
+    // フッターを最下部に固定
     var $ftr = $('#footer');
     if( window.innerHeight > $ftr.offset().top + $ftr.outerHeight() ){
       $ftr.attr({'style': 'position:fixed; top:' + (window.innerHeight - $ftr.outerHeight()) +'px;' });
@@ -60,6 +61,30 @@
     $switchImgSubs.on('click',function(e){
       $switchImgMain.attr('src',$(this).attr('src'));
     });
+
+    // お気に入り登録・削除
+    var $like,
+        likeProductId;
+    $like = $('.js-click-like') || null; //nullというのはnull値という値で、「変数の中身は空ですよ」と明示するためにつかう値
+    likeProductId = $like.data('productid') || null;
+    // 数値の0はfalseと判定されてしまう。product_idが0の場合もありえるので、0もtrueとする場合にはundefinedとnullを判定する
+    if(likeProductId !== undefined && likeProductId !== null){
+      $like.on('click',function(){
+        var $this = $(this);
+        $.ajax({
+          type: "POST",
+          url: "ajaxLike.php",
+          data: { productId : likeProductId}
+        }).done(function( data ){
+          console.log('Ajax Success');
+          // クラス属性をtoggleでつけ外しする
+          $this.toggleClass('active');
+        }).fail(function( msg ) {
+          console.log('Ajax Error');
+        });
+      });
+    }
+
   });
 </script>
 </body>
