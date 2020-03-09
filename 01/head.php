a5c1bedeaaa8dc28d6b806ead03ab52d697d50f8
kazunari.kaneko <kaneko.kazunari0206@gmail.com>
2020-03-09 23:00:25 +0900

    font-awesom追加

--- 01/head.php
+++ 01/head.php
@@ -6,4 +6,7 @@
   <title><?php echo $siteTitle; ?> | WEBKATU MARKET</title>
   <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Montserrat:400,700">
+  <!-- フォントアイコン -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+
 </head>
