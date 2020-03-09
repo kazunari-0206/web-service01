d6981262c054d64fc8c14651e2ca919683ca3f53
kazunari.kaneko <kaneko.kazunari0206@gmail.com>
2020-03-09 12:17:27 +0900

    10画面処理、HTML部分完成

--- 01/productDetail.php
+++ 01/productDetail.php
@@ -1,4 +1,68 @@
 <?php
+
+//共通変数・関数ファイルを読み込み
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug(' 商品詳細ページ ');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+
+//================================
+// 画面処理
+//================================
+
+// 画面表示用データ取得
+//================================
+// 商品IDのGETパラメータを取得
+$p_id = (!empty($_GET['p_id'])) ? $_GET['p_id'] : '';
+debug('$p_idの中身：'.print_r($p_id, true));
+// DBから商品データを取得
+$viewData = getProductOne($p_id);
+debug('$viewDataの中身：'.print_r($viewData, true));
+// パラメータに不正な値が入っているかチェック
+if(empty($viewData)){
+  error_log('エラー発生：指定ページに不正な値が入りました');
+  header("Location:index.php"); //トップページへ
+  exit;
+}
+debug('取得したDBデータ：'.print_r($viewData, true));
+
+// post送信されていた場合
+if(!empty($_POST['submit'])){
+  debug('POST送信があります。');
+
+  //ログイン認証
+  require('auth.php');
+
+  //例外処理
+  try {
+    // DBへ接続
+    $dbh = dbConnect();
+    // SQL文作成
+    $sql = 'INSERT INTO bord (sale_user, buy_user, product_id, create_date) VALUES (:s_uid, :b_uid, :p_id, :date)';
+    $data = array(':s_uid' => $viewData['user_id'], 'b_uid' => $_SESSION['user_id'], ':p_id' => $p_id, ':date' => date('Y-m-d H:i:s'));
+    // クエリ実行
+    $stmt = queryPost($dbh, $sql, $data);
+
+    // クエリ成功の場合
+    if($stmt){
+      $_SESSION['msg_success'] = SUC05;
+      debug('連絡掲示板へ遷移します。');
+      header("Location:msg.php?m_id=".$dbh->lastInsertID()); //連絡掲示板へ
+      exit;
+    }
+
+  } catch (Exception $e) {
+    error_log('エラー発生:' . $e->getMessage());
+    $err_msg['common'] = MSG07;
+  }
+}
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<');
+?>
+
+<?php
 $siteTitle = '商品詳細';
 require('head.php'); 
 ?>
@@ -75,18 +139,10 @@ require('head.php');
       }
     </style>
 
-    <!-- メニュー -->
-    <header>
-      <div class="site-width">
-        <h1><a href="index.php">WEBUKATU MARKET</a></h1>
-        <nav id="top-nav">
-          <ul>
-            <li><a href="signup.php" class="btn btn-primary">ユーザー登録</a></li>
-            <li><a href="">ログイン</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <!-- ヘッダー -->
+    <?php
+      require('header.php');
+    ?>
 
     <!-- メインコンテンツ -->
     <div id="contents" class="site-width">
@@ -95,41 +151,36 @@ require('head.php');
       <section id="main" >
 
         <div class="title">
-          <span class="badge">スマホ</span>
-          iPhone6s
+          <span class="badge"><?php echo sanitize($viewData['category']); ?></span>
+          <?php echo sanitize($viewData['name']); ?>
         </div>
         <div class="product-img-container">
           <div class="img-main">
-            <img src="/img/sample01.jpg" alt="">
+            <img src="<?php echo showImg(sanitize($viewData['pic1'])); ?>" alt="メイン画像：<?php echo sanitze($viewData['name']); ?>" id="js-switch-img-main">
           </div>
           <div class="img-sub">
-            <img src="/img/sample02.jpg" alt="">
-            <img src="/img/sample03.gif" alt="">
-            <img src="/img/sample04.jpg" alt="">
+            <img src="<?php echo showImg(sanitize($viewData['pic1'])); ?>" alt="画像1：<?php echo sanitze($viewData['name']); ?>" class="js-switch-img-sub">
+            <img src="<?php echo showImg(sanitize($viewData['pic2'])); ?>" alt="画像2：<?php echo sanitze($viewData['name']); ?>" class="js-switch-img-sub">
+            <img src="<?php echo showImg(sanitize($viewData['pic3'])); ?>" alt="画像3：<?php echo sanitze($viewData['name']); ?>" class="js-switch-img-sub">        
           </div>
         </div>
         <div class="product-detail">
           <p>
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
+            <?php echo sanitize($viewData['comment']); ?>
           </p>
         </div>
         <div class="product-buy">
           <div class="item-left">
-            <a href="index.php">&lt; 商品一覧に戻る</a>
+            <a href="index.php<?php appendGetParam(array('p_id')); ?>">&lt; 商品一覧に戻る</a>
           </div>
-          <div class="item-right">
-            <button class="btn btn-primary">買う!</button>
-          </div>
-          <div class="item-right">
-            <p class="price">¥85,000-</p>
-          </div>
+          <form action="" method="post"> <!-- formタブを追加し、ボタンをinputに変更し、style追加 -->
+            <div class="item-right">
+              <input type="submit" value="買う！" name="submit" class="btn btn-primary" style="margin-top:0;">
+            </div>
+            <div class="item-right">
+              <p class="price">¥<?php echo sanitize(number_format($viewData['price'])); ?>-</p>
+            </div>
+          </form>
         </div>
 
       </section>
